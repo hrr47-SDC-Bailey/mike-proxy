bf344272eb3db6b18252a12d83c7ed08480bb662
Mike Shertz <shertzm2@gmail.com>
2020-09-08 18:13:29 -0400

    deployed proxy should now work

--- server/index.js
+++ server/index.js
@@ -9,13 +9,13 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
-// app.use('/hostels/:hostel_id', express.static(path.join(__dirname, '../public')));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use('/hostels/:hostel_id', express.static(path.join(__dirname, '../public')));
+//app.use(express.static(path.join(__dirname, '../public')));
 
 // API reroutes
 
 // Image carousel service
-//app.use('/api/hostels/:hostel_id/images', createProxyMiddleware({ target: 'http://3.135.180.53:3007', changeOrigin: true }));
+app.use('/api/hostels/:hostel_id/images', createProxyMiddleware({ target: 'http://3.135.180.53:3007', changeOrigin: true }));
 // availability service
 // app.use('/api/hostel/:hostelId/rooms', createProxyMiddleware({ target: 'http://localhost:3009', changeOrigin: true }));
 // // description map rules service
